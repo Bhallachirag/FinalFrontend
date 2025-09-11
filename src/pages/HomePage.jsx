63846ca7574ed589590e8e743daf4b15e9ae298d
Chirag Bhalla <chiragbhalla73@gmail.com>
2025-09-11 16:48:42 +0530

    Again updated ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -69,7 +69,7 @@ const HomePage = () => {
 
     return (
       <div className="fixed inset-0 bg-opacity-50 z-50 flex items-center justify-center p-4">
-        <div className="bg-white rounded-2xl p-6 max-w-md w-full relative">
+        <div className="bg-rose-300 rounded-2xl p-6 max-w-md w-full relative">
           <button
             onClick={onClose}
             className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
